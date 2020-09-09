aab366466b91903eb4df258cf7e08a91a705c924
Matthew Clark <matthew.clark.dev@gmail.com>
2020-09-09 12:55:50 -0500

    Approximation of working AST.

--- src/ast.rs
+++ src/ast.rs
@@ -1,10 +1,8 @@
 use crate::data::LispDatum;
-use std::fmt::{Debug, Formatter};
+use std::fmt::{Display, Formatter};
 use std::fmt;
-use std::collections::HashMap;
 use crate::parse::Statement;
 use crate::ast::ASTNode::{Literal, Call};
-use std::ops::Deref;
 
 // NOTE(matthew-c21): Special forms to be added here.
 pub enum ASTNode {
@@ -44,19 +42,19 @@ impl ASTNode {
         ASTNode::from_index(statements, 0)
     }
 
-    pub fn accept(&self, visitor: &mut Box<dyn ASTVisitor>) {
+    pub fn accept(&self, visitor: &mut dyn ASTVisitor) {
         match self {
-            ASTNode::Literal(_) => visitor.visit_literal(self),
-            ASTNode::Call(_, _) => visitor.visit_call(self),
+            ASTNode::Literal(d) => visitor.visit_literal(d),
+            ASTNode::Call(c, a) => visitor.visit_call(c, a),
         }
     }
 }
 
 // NOTE(matthew-c21): This is subject to change in response to special forms.
 pub trait ASTVisitor {
-    fn visit_literal(&mut self, node: &ASTNode);
+    fn visit_literal(&mut self, node: &LispDatum);
 
-    fn visit_call(&mut self, node: &ASTNode);
+    fn visit_call(&mut self, callee: &ASTNode, args: &Vec<ASTNode>);
 }
 
 // All optimizers should be in the form ASTNode -> ASTNode.
@@ -65,13 +63,13 @@ pub trait ASTVisitor {
 //  need to break it up to account for functions and (possibly) imports.
 fn preamble() -> String {
     String::from(
-        "#include \"lisp.h\"\
+        "#include \"lisp.h\"\n\
         int main() {\
         ")
 }
 
 fn postamble() -> String {
-    String::from("}")
+    String::from("return 0;}")
 }
 
 pub struct TranspilationVisitor {
@@ -85,52 +83,40 @@ impl TranspilationVisitor {
 }
 
 impl ASTVisitor for TranspilationVisitor {
-    fn visit_literal(&mut self, node: &ASTNode) {
+    fn visit_literal(&mut self, node: &LispDatum) {
         let content = match node {
-            Literal(x) => {
-                match x {
-                    LispDatum::Cons(a, b) => unimplemented!(),  // TODO(matthew-c21): I've realized that this is a terrible way to go about doing things.
-                    LispDatum::Complex(r, i) => format!("new_complex({},{})", r, i),
-                    LispDatum::Real(x) => format!("new_real({})", x),
-                    LispDatum::Rational(p, q) => format!("new_rational({},{})", p, q),
-                    LispDatum::Integer(i) => format!("new_integer({})", i),
-                    LispDatum::Symbol(s) => format!("new_symbol({})", s),
-                    LispDatum::Nil => format!("get_nil()"),
-                }
-            }
-            Call(_, _) => { unreachable!() }
+            LispDatum::Cons(a, b) => unimplemented!(),
+            LispDatum::Complex(r, i) => format!("new_complex({},{})", r, i),
+            LispDatum::Real(x) => format!("new_real({})", x),
+            LispDatum::Rational(p, q) => format!("new_rational({},{})", p, q),
+            LispDatum::Integer(i) => format!("new_integer({})", i),
+            LispDatum::Symbol(s) => format!("new_symbol({})", s),
+            LispDatum::Nil => format!("get_nil()"),
         };
 
         self.content.push_str(content.as_str());
     }
 
-    fn visit_call(&mut self, node: &ASTNode) {
-        match node {
-            Literal(_) => unreachable!(),
-            Call(b, args) => {
-                match b.deref() {
-                    Literal(LispDatum::Symbol(x)) => {
-                        let mut out = String::from(x);
-                        let args: Vec<String> = args.iter().map(|arg| arg.accept(Box::new(self))).collect();
-                        out.push('(');
-                        for arg in args {
-                            out.push_str(arg.as_str());
-                            out.push(',');
-                        }
+    fn visit_call(&mut self, callee: &ASTNode, args: &Vec<ASTNode>) {
+        let content = match callee {
+            Literal(LispDatum::Symbol(s)) => {
+                let mut out = format!("{}(", s);
 
-                        out.push(')');
+                args.iter().for_each(|arg| {
+                    arg.accept(self);
+                    out.push(',');
+                });
 
-                        out
-                    },
-                    _ => unimplemented!()
-                }
-            }
+                out
+            },
             _ => unimplemented!()
-        }
+        };
+
+        self.content.push_str(content.as_str());
     }
 }
 
-impl Debug for TranspilationVisitor {
+impl Display for TranspilationVisitor {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{}{}{}", preamble(), self.content, postamble())
     }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
+use crate::ast::TranspilationVisitor;
+
 mod lex;
 mod parse;
 mod data;
 mod ast;
 
-#[link(name = "lisp", kind = "static")]
-extern "C" {
-    fn _rust_demo(v: f32);
-}
-
 fn main() {
-    println!("Hello, world!");
-    unsafe { _rust_demo(2. * std::f32::consts::PI) }
-    parse::parse(lex::start("").unwrap());
+    let tokens = lex::start("(* 1 2 3)").unwrap();
+    let parse_tree = parse::parse(tokens).unwrap();
+    let ast = ast::ASTNode::from(parse_tree).unwrap();
+    let mut visitor = TranspilationVisitor::new();
+
+    for node in ast {
+        node.accept(&mut visitor);
+    }
+
+    println!("{}", visitor);
 }
