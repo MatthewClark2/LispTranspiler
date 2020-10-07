c41ad8ffc3234721795e3fdf0a24c706341bade9
Matthew Clark <matthew.clark.dev@gmail.com>
2020-10-07 15:58:58 -0500

    Update project version.

--- lisp/example.lisp
+++ lisp/example.lisp
@@ -1,11 +1,7 @@
 (format 3i)
 
-(format (+ 1 (* 3 3)))
+(format (+ 1 1))
 
-(format (* 1 1))
+(format (+ 2 5/2))
 
-(format (* 2 5))
-
-(format (* (- 3) (- 6)))
-
-(format (* 2.5 (- 4)))
+(format (+ 2.5 (+ 4 2i)))
