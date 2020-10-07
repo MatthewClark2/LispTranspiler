7c497b36f2d3c3522f7461786d6d6a05aece4530
Matthew Clark <matthew.clark.dev@gmail.com>
2020-10-07 15:51:04 -0500

    Break Rust's dependency on C library.

--- build.rs
+++
@@ -1,8 +0,0 @@
-extern crate cmake;
-use cmake::Config;
-
-fn main() {
-    let dst = Config::new(".").build();
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=lisp");
-}
