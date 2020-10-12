a2132d23a0fe6e85785b75cf4502cf5432f8086f
Matthew Clark <matthew.clark.dev@gmail.com>
2020-10-12 18:02:13 -0500

    Fill out tests for multiplication and equality. Fix test generation.

--- liblisp/test/test_stdlib.c
+++ liblisp/test/test_stdlib.c
@@ -161,15 +161,18 @@ void Test_rational_equality(CuTest* tc) {
 }
 
 void Test_rational_inequality(CuTest* tc) {
-  tc = NULL;
+  CuAssert(tc, "1/2 != -1/2", !eqv(new_rational(1, 2), new_rational(-1, 2)));
+  CuAssert(tc, "3/4 != 1/2", !eqv(new_rational(3, 4), new_rational(1, 2)));
 }
 
 void Test_complex_equality(CuTest* tc) {
-  tc = NULL;
+  CuAssert(tc, "1+2i = 1+2i", eqv(new_complex(1, 2), new_complex(1, 2)));
 }
 
 void Test_complex_inequality(CuTest* tc) {
-  tc = NULL;
+  CuAssert(tc, "1+2i != -1+2i", !eqv(new_complex(1, 2), new_complex(-1, 2)));
+  CuAssert(tc, "3+4i != 1+2i", !eqv(new_complex(3, 4), new_complex(1, 2)));
+  CuAssert(tc, "1-i != 1+i", !eqv(new_complex(1, -1), new_complex(1, 1)));
 }
 
 void Test_promotion_equality_int(CuTest* tc) {
@@ -261,7 +264,6 @@ void Test_promotion_equality_real(CuTest* tc) {
   discard_datum(a);
   discard_datum(b);
 }
-
 
 void Test_cons_equality(CuTest* tc) {
   tc = NULL;
@@ -478,33 +480,80 @@ void Test_real_complex_multiplication(CuTest* tc) {
 }
 
 void Test_rational_int_multiplication(CuTest* tc) {
-  tc = NULL;
+  struct LispDatum* args[2];
+  args[0] = new_rational(2, 3);
+  args[1] = new_integer(5);
+
+  struct LispDatum* result = multiply(args, 2);
+  CuAssertIntEquals(tc, 6, result->num);
+  CuAssertIntEquals(tc, 1, result->den);
 }
 
 void Test_rational_real_multiplication(CuTest* tc) {
-  tc = NULL;
+  struct LispDatum* args[2];
+  args[0] = new_rational(-2, 5);
+  args[1] = new_real(2.5);
+
+  CuAssert(tc, "-2/5 * 2.5 = -1.0", eqv(multiply(args, 2), new_real(-1.0)));
 }
 
 void Test_rational_rational_multiplication(CuTest* tc) {
-  tc = NULL;
+  struct LispDatum* args[3];
+  args[0] = new_rational(2, 3);
+  args[1] = new_rational(3, 4);
+
+  struct LispDatum* result = multiply(args, 2);
+  CuAssert(tc, "2/3 * 3/4 is rational", result->type = Rational);
+  CuAssertIntEquals(tc, 1, result->num);
+  CuAssertIntEquals(tc, 2, result->den);
 }
 
 void Test_rational_complex_multiplication(CuTest* tc) {
-  tc = NULL;
+  struct LispDatum* args[2];
+  args[0] = new_rational(-7, 2);
+  args[1] = new_complex(2, -5.2);
+
+  CuAssert(tc, "-7/2 * 2-5.2i = -7+18.2i", eqv(multiply(args, 2), new_complex(-7, 18.2)));
 }
 
 void Test_complex_int_multiplication(CuTest* tc) {
-  tc = NULL;
+  struct LispDatum* args[2];
+  args[0] = new_complex(-2, 5);
+  args[1] = new_integer(4);
+
+  CuAssert(tc, "-2+5i * 4 = -8+20i", eqv(multiply(args, 2), new_complex(-8, 20)));
 }
 
 void Test_complex_real_multiplication(CuTest* tc) {
-  tc = NULL;
+  struct LispDatum* args[2];
+  args[0] = new_complex(-2, 5);
+  args[1] = new_real(-0.5);
+
+  CuAssert(tc, "-2+5i * -0.5 = 1-2.5i", eqv(multiply(args, 2), new_complex(1, -2.5)));
 }
 
 void Test_complex_rational_multiplication(CuTest* tc) {
-  tc = NULL;
+  struct LispDatum* args[2];
+  args[0] = new_complex(-2, 5);
+  args[1] = new_rational(-1, 2);
+
+  CuAssert(tc, "-2+5i * -1/2 = 1-2.5i", eqv(multiply(args, 2), new_complex(1, -2.5)));
 }
 
 void Test_complex_complex_multiplication(CuTest* tc) {
-  tc = NULL;
+  struct LispDatum* args[2];
+  args[0] = new_complex(-2, 5);
+  args[1] = new_complex(-2, -5);
+
+  CuAssert(tc, "-2+5i * -2-5i = -4", eqv(multiply(args, 2), new_complex(29, 0)));
+}
+
+void Test_multiplication(CuTest* tc) {
+  struct LispDatum* args[4];
+  args[0] = new_integer(3);
+  args[1] = new_rational(1, 2);
+  args[2] = new_real(-3.5);
+  args[3] = new_complex(-4.25, 6.125);
+
+  CuAssert(tc, "3 * 1/2 * -3.5 * -4.25+6.125i = ", eqv(multiply(args, 4), new_complex(22.3125, -32.15625)));
 }
