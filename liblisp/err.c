75ebb477b9fd6d3e46036680d04f2fd3fda1a41d
Matthew Clark <matthew.clark.dev@gmail.com>
2021-01-23 12:00:11 -0600

    Expand error management.

--- liblisp/err.c
+++ liblisp/err.c
@@ -3,16 +3,50 @@
 
 #include "err.h"
 
+/**
+ * Error state value. This value should be treated as readonly by client code. Should multi-threaded behavior ever
+ * become a thing, this will need to be more complex.
+ */
+static enum Cause GlobalErrorState = None;
+
+static enum ErrorBehavior GlobalErrorBehavior = LogOnly;
+
+static void destroy_and_exit() {
+  // TODO(matthew-c21): If necessary, add resource handles here to be closed before exiting.
+  exit(-1);
+}
+
 static char* cause_string(enum Cause cause) {
   switch (cause) {
-    case ZeroDivision: return "Division by Zero";
-    case Math: return "Math Exception";
-    case Generic: return "Runtime Exception";
-    default: return "Unknown exception";
+    case ZeroDivision:
+      return "Division by Zero";
+    case Math:
+      return "Math Exception";
+    case Generic:
+      return "Runtime Exception";
+    case Type:
+      return "Type Mismatch Exception";
+    case Argument:
+      return "Invalid Argument Exception";
+    default:
+      return "Unknown exception";
   }
 }
 
 void* raise(enum Cause cause, const char* msg) {
   fprintf(stderr, "%s: %s\n", cause_string(cause), msg);
-  exit(-1);
+  GlobalErrorState = cause;
+
+  switch (GlobalErrorBehavior) {
+    case LogAndQuit:
+      destroy_and_exit();
+      break;
+    case LogOnly:
+      break;
+    default:
+      fprintf(stderr, "Illegal error behavior detected. Exiting immediately.");
+      destroy_and_exit();
+  }
+
+  return NULL;
 }
