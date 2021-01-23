24305ea11c11aaf81f2a9839adecf8de9448cc8d
Matthew Clark <matthew.clark.dev@gmail.com>
2021-01-23 03:34:12 -0600

    Unit testing and suggested error update.

--- liblisp/err.h
+++ liblisp/err.h
@@ -1,9 +1,22 @@
 #ifndef LISP_ERR_H
 #define LISP_ERR_H
 
+// TODO(matthew-c21): Add more descriptive causes.
 enum Cause {
   ZeroDivision, Math, Generic
 };
+
+// TODO(matthew-c21): Modify to return NULL. Possibly use it to trigger some global error state that needs to be primed
+//  in generated code in order to quit the program.
+/*
+ * i.e.
+ *
+ * void* raise(cause, msg) { trip_error(cause, msg); return NULL: }
+ *
+ * void trip_error(cause, msg) { (* details omitted *) if (primed) exit(-1); }
+ *
+ */
+
 
 /**
  * Basic means of expounding on runtime errors. Prints cause and message to stderr, then exits program. While no value
