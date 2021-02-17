60a1d4ae8b4ea6e9e19f396b650d258fce856737
Matthew Clark <matthew.clark.dev@gmail.com>
2021-02-16 20:42:17 -0600

    Update parse testing.

--- src/parse.rs
+++ src/parse.rs
@@ -6,7 +6,7 @@ pub enum ParseTree {
     Branch(Vec<ParseTree>),
 }
 
-pub fn parse(tokens: Vec<Token>) -> Result<Vec<ParseTree>, (u32, String)> {
+pub fn parse(tokens: &Vec<Token>) -> Result<Vec<ParseTree>, (u32, String)> {
     let mut statements: Vec<ParseTree> = Vec::new();
     let mut t = &tokens[..];
 
@@ -50,24 +50,24 @@ fn list(tokens: &[Token]) -> Result<(ParseTree, &[Token]), (u32, String)> {
 #[cfg(test)]
 mod test {
     use crate::parse::parse;
-    use crate::lex::{Token, TokenValue};
+    use crate::lex::{Token, TokenValue::*, start};
     use crate::parse::ParseTree::{Leaf, Branch};
 
     #[test]
     fn single_terminal() {
-        let tokens = vec!(Token::from(TokenValue::Int(16)));
+        let tokens = vec!(Token::from(Int(16)));
 
-        let x = parse(tokens).unwrap();
+        let x = parse(&tokens).unwrap();
 
         assert_eq!(x.len(), 1);
-        assert_eq!(x[0], Leaf(Token::from(TokenValue::Int(16))));
+        assert_eq!(x[0], Leaf(Token::from(Int(16))));
     }
 
     #[test]
     fn empty_list() {
-        let tokens = vec!(Token::from(TokenValue::Open), Token::from(TokenValue::Close));
+        let tokens = vec!(Token::from(Open), Token::from(Close));
 
-        let x = parse(tokens).unwrap();
+        let x = parse(&tokens).unwrap();
 
         assert_eq!(x.len(), 1);
         assert_eq!(x[0], Branch(Vec::new()));
@@ -76,13 +76,13 @@ mod test {
     #[test]
     fn list_of_terminals() {
         let tokens = vec!(
-            Token::from(TokenValue::Open),
-            Token::from(TokenValue::Symbol("+".to_string())),
-            Token::from(TokenValue::Int(16)),
-            Token::from(TokenValue::Int(4)),
-            Token::from(TokenValue::Close));
+            Token::from(Open),
+            Token::from(Symbol("+".to_string())),
+            Token::from(Int(16)),
+            Token::from(Int(4)),
+            Token::from(Close));
 
-        let x = parse(tokens).unwrap();
+        let x = parse(&tokens).unwrap();
 
         assert_eq!(x.len(), 1);
 
@@ -90,9 +90,9 @@ mod test {
             Leaf(_) => assert!(false),
             Branch(x) => {
                 assert_eq!(x.len(), 3);
-                assert_eq!(x[0], Leaf(Token::from(TokenValue::Symbol("+".to_string()))));
-                assert_eq!(x[1], Leaf(Token::from(TokenValue::Int(16))));
-                assert_eq!(x[2], Leaf(Token::from(TokenValue::Int(4))));
+                assert_eq!(x[0], Leaf(Token::from(Symbol("+".to_string()))));
+                assert_eq!(x[1], Leaf(Token::from(Int(16))));
+                assert_eq!(x[2], Leaf(Token::from(Int(4))));
             }
         }
     }
@@ -100,17 +100,17 @@ mod test {
     #[test]
     fn nested_list() {
         let tokens = vec!(
-            Token::from(TokenValue::Open),
-            Token::from(TokenValue::Open),
-            Token::from(TokenValue::Close),
-            Token::from(TokenValue::Open),
-            Token::from(TokenValue::Open),
-            Token::from(TokenValue::Close),
-            Token::from(TokenValue::Close),
-            Token::from(TokenValue::Close),
+            Token::from(Open),
+            Token::from(Open),
+            Token::from(Close),
+            Token::from(Open),
+            Token::from(Open),
+            Token::from(Close),
+            Token::from(Close),
+            Token::from(Close),
         );
 
-        let x = parse(tokens).unwrap();
+        let x = parse(&tokens).unwrap();
 
         assert_eq!(x.len(), 1);
 
@@ -124,54 +124,51 @@ mod test {
         }
     }
 
-    /*
     #[test]
-    #[ignore]
     fn small_comprehensive() {
-        let tokens = lex::start("a 12 d1- (* 1i 2. (+ x 3)) ()").unwrap();
-        let x = parse(tokens).unwrap();
+        let tokens = start("a 12 d1- (* 1i 2. (+ x 3)) ()").unwrap();
+        let x = parse(&tokens).unwrap();
 
         assert_eq!(x.len(), 5);
-        assert_eq!(x[0], Terminal(Symbol("a".to_string())));
-        assert_eq!(x[1], Terminal(Integer(12)));
-        assert_eq!(x[2], Terminal(Symbol("d1-".to_string())));
+        assert_eq!(x[0], Leaf(tokens[0].clone()));
+        assert_eq!(x[1], Leaf(tokens[1].clone()));
+        assert_eq!(x[2], Leaf(tokens[2].clone()));
         match &x[3] {
-            Terminal(_) => panic!(),
-            List(x) => {
+            Leaf(_) => panic!(),
+            Branch(x) => {
                 assert_eq!(x.len(), 4);
-                assert_eq!(x[0], Terminal(Symbol("*".to_string())));
-                assert_eq!(x[1], Terminal(Complex(0., 1.)));
-                assert_eq!(x[2], Terminal(Real(2.)));
+                assert_eq!(x[0], Leaf(tokens[4].clone()));
+                assert_eq!(x[1], Leaf(tokens[5].clone()));
+                assert_eq!(x[2], Leaf(tokens[6].clone()));
 
                 match &x[3] {
-                    Terminal(_) => panic!(),
-                    List(x) => {
+                    Leaf(_) => panic!(),
+                    Branch(x) => {
                         assert_eq!(x.len(), 3);
 
-                        assert_eq!(x[0], Terminal(Symbol("+".to_string())));
-                        assert_eq!(x[1], Terminal(Symbol("x".to_string())));
-                        assert_eq!(x[2], Terminal(Integer(3)));
+                        assert_eq!(x[0], Leaf(tokens[8].clone()));
+                        assert_eq!(x[1], Leaf(tokens[9].clone()));
+                        assert_eq!(x[2], Leaf(tokens[10].clone()));
                     }
                 }
             }
         }
 
         match &x[4] {
-            Terminal(_) => panic!(),
-            List(x) => {
+            Leaf(_) => panic!(),
+            Branch(x) => {
                 assert_eq!(x.len(), 0);
             }
         }
     }
-    */
 
     #[test]
     #[should_panic]
     fn fails_unbalanced_parens() {
-        parse(vec!(
-            Token::from(TokenValue::Open),
-            Token::from(TokenValue::Open),
-            Token::from(TokenValue::Close),
+        parse(&vec!(
+            Token::from(Open),
+            Token::from(Open),
+            Token::from(Close),
         )).unwrap();
     }
 }
