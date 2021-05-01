18cf7ae1741da15fc6d28508b0c94e9e34749aab
Matthew Clark <matthew.clark.dev@gmail.com>
2021-05-01 12:45:53 -0500

    Update demo for final presentation.

--- lisp/demo/demo.lisp
+++ lisp/demo/demo.lisp
@@ -17,3 +17,25 @@
 
 ;; They can also be used as values.
 (format "Are empty lists false? " (if (list) "no" "yes"))
+
+;; Function fanciness.
+
+;; Lambdas have no natural recursion, so it needs to be packaged like so.
+(define rfncall (lambda (f . args) (apply f (cons f args))))
+
+;; Utility function that retains accumulator and recursive form.
+(define map-aux (lambda (rec f alist out)
+  (if (eqv (length alist) 0)
+      (reverse out)
+      (funcall rfncall 
+	       rec
+	       f
+	       (cdr alist)
+	       (cons (funcall f (car alist))
+		     out)))))
+
+(define map (lambda (f alist)
+	      (funcall rfncall map-aux f alist nil)))
+
+(format (funcall map (lambda (x) (+ 5 x)) (list 1 2 3 4)))
+
